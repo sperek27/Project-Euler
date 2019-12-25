e10017e21ae1df30357677ef3b628bb9ec31eadb
sperek27 <50334639+sperek27@users.noreply.github.com>
2019-12-25 13:18:25 -0600

    Create ProjectEuler_1.py

---
+++ ProjectEuler_1.py
@@ -0,0 +1,5 @@
+print(999//3) #333
+print(999//5) #199
+print(999//15) #66
+# Principle of Inclusion Exclusion and the triangle number formula gives the correct answer
+print(3*333*334//2 + 5*199*200//2 - 15*66*67//2)
