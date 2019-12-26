0574dc1b365195735ff2640fed341659563e8bb2
sperek27 <50334639+sperek27@users.noreply.github.com>
2019-12-26 13:31:27 -0600

    Update ProjectEuler_36.py

--- ProjectEuler_36.py
+++ ProjectEuler_36.py
@@ -1,10 +1,12 @@
 def reverse(s):
   return s[::-1]
+
 def pal(s):
   if str(s) == reverse(str(s)):
     return True
   else:
     return False
+  
 def binary(n):
   q=0
   while n>0:
@@ -14,10 +16,11 @@ def binary(n):
     n=n-(2**(z-1))
     q=q+(10**(z-1))
   return q
+
 a=1
 count = 0
 while a < 1000000:
-  if pal(a) == True & pal(binary(a)) == True:
+  if pal(a) == True and pal(binary(a)) == True:
     count = count + a
   a=a+1
 print(count)
