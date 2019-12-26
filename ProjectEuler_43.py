e75aa65d91354c915f31903bc899a5ad29f306bf
sperek27 <50334639+sperek27@users.noreply.github.com>
2019-12-25 23:50:30 -0600

    Update ProjectEuler_43.py

--- ProjectEuler_43.py
+++ ProjectEuler_43.py
@@ -7,6 +7,5 @@ for j in l:
   for L in j:
     i += str(L)
   if int(i[7:10])%17 == 0 and int(i[6:9])%13 == 0 and int(i[5:8])%11 == 0 and int(i[4:7])%7 == 0 and int(i[3:6])%5 == 0 and int(i[2:5])%3 == 0 and int(i[1:4])%2 == 0:
-    print(i)
     s += int(i)
 print(s)
